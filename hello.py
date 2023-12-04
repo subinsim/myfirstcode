16bd198264fc719525d32ea9ef92d863eaf4a65d
subinsim <144295629+subinsim@users.noreply.github.com>
2023-12-04 22:56:29 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
-print("Hello World")
+print("Hello World")   #"Hello World"라는 문자열을 출력하는 함수입니다. 따라서 이 코드를 실행하면 "Hello World"가 출력됩니다.
 Hello World
 \
-"Hello World"
-'Hello World'
-4+4
+"Hello World"    #큰 따옴표로 묶인 "Hello World"라는 문자열입니다.
+'Hello World'    #작은 따옴표로 묶인 "Hello World"라는 문자열입니다.
+4+4    #두 개의 정수를 더하는 연산입니다. 따라서 이 코드를 실행하면 8이 출력됩니다.
 8
-Hello World
+Hello World    #이 줄은 코드가 아니라 그냥 문자열입니다. 따라서 이 코드를 실행하면 에러가 발생합니다.
 SyntaxError: incomplete input
